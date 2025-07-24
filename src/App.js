e34a18e15a29ab83621387fd60789d89714b7d76
srjient <serjio.santana@outlook.com>
2025-07-24 18:01:12 +0200

    5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import './styles.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        
-      </header>
+     <div className='container'>
+      <Header></Header>
+      <MoviesGrid></MoviesGrid>
+      </div> 
+
+
+      <Footer></Footer>         
     </div>
   );
 }
