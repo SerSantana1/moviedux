6e73fb63685ee77f1f5b6538876bf093df13fea7
Srjnt_hm <santanaserjio@gmail.com>
2025-07-28 23:50:26 +0200

    lessonn 5

---
+++ .history/src/components/MoviesGrid_20250726125216.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from "react";
+import '../styles.css';
+
+
+export default function MoviesGrid(){
+
+    const [movies, setMovies] = useState([]);
+
+ 
+
+    useEffect(() => {
+
+        fetch("movies.json")
+        console.log( fetch("movies.json").then(response => Response.json()))
+        .then(response => Response.json() )
+        .then(data => setMovies(data))
+    
+
+    },[])
+
+
+    return(<div>{movies.length}</div>)
+}
--- .history/src/components/MoviesGrid_20250726143533.js
+++ .history/src/components/MoviesGrid_20250726143533.js
@@ -22,11 +22,6 @@ export default function MoviesGrid(){
     movies.map(movie => (
         <div key={movie.id} className="movie-card"> 
         <img src={`images/${movie.image}`} alt={movie.titleS}></img>
-        <div className="movie-card-info">
-            <h3 className="movie-card-title">{movie.title}</h3>
-            <p className="movie-card-genre">{movie.genre}</p>
-            <p className="movie-card-rating">{movie.rating}</p>
-        </div>
         </div>
     ))
 }
